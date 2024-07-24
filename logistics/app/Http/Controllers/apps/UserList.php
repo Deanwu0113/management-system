9e3d97ab0f4dd4a726194ae9f639d5df1e4d0fed
Erica_Xu_1102745 <Wencong.xu@lincolnuni.ac.nz>
2024-07-24 18:31:31 +1200

    update user list with database, link user to usermanagement

--- logistics/app/Http/Controllers/apps/UserList.php
+++ logistics/app/Http/Controllers/apps/UserList.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\apps;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserList extends Controller
 {
-  public function index()
-  {
-    return view('content.apps.app-user-list');
-  }
+    public function index()
+    {
+        $users = User::all(); // 获取所有用户数据
+        return view('content.apps.app-user-list', compact('users'));
+    }
 }
